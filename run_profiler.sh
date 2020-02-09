f40e234561dc721897914a6f3884ceaa8c1c6e75
MatrosovaTalia <zamoklaputa@gmail.com>
2020-02-09 22:33:50 +0300

    add kotlin time and memeory check

--- run_profiler.sh
+++ run_profiler.sh
@@ -26,10 +26,10 @@ do
 done
 
 # for c compilation and execution
-
 for f in "c"/*.c
 do
 	echo "Profiling $f" >> $res
+	echo "$f" >> $progs
 	gcc $f -o ${f%.c}.out
 	{ time ./"${f%.c}.out" ; } 2>> $res
 	{ /usr/bin/time -f "\t%M max memory consumption" ./"${f%.c}.out" ; } 2>> $res
@@ -39,12 +39,25 @@ do
 	
 done
 
-# for java execution 
+# for kotlin compilation and execution
+for f in "kt"/*.kt
+do
+	echo "Profiling $f" >> $res
+	echo "$f" >> $progs
+	kotlinc $f -include-runtime -d ${f%.kt}.jar
+	{ time java -jar ${f%.kt}.jar ; } 2>> $res
+	{ /usr/bin/time -f "\t%M max memory consumption" java -jar ${f%.kt}.jar ; } 2>> $res
+	echo "" >> $res
 
+done
+
+
+# for java execution 
 for f in "java"/*.java
 do
 	echo "Profiling $f" >> $res
 	{ time java $f ; } 2>> $res
+	echo "$f" >> $progs
 	{ /usr/bin/time -f "\t%M max memory consumption" java $f ; } 2>> $res
 	echo "" >> $res
 done
