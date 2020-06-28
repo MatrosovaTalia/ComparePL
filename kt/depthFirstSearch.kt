e8342585c24927f3f050931e12b5447e0e49468c
Picroc <kingofrootelement@gmail.com>
2020-06-28 17:05:30 +0300

    Fixed Kotlin anomaly

--- kt/depthFirstSearch.kt
+++ kt/depthFirstSearch.kt
@@ -1,51 +1,39 @@
-class Graph {
-    private data class Vertex(val name: Int) {
-        val neighbors = mutableSetOf<Vertex>()
+class Node(var dat: Int) {
+    var visited: Boolean = false
+    var neighbours = mutableListOf<Node>()
+
+    fun addneighbours(neighbourNode: Node) {
+        this.neighbours.add(neighbourNode)
     }
-    private val vertices = mutableMapOf<Int, Vertex>()
-    private fun connect(first: Vertex?, second: Vertex?) {
-        if (second != null) first?.neighbors?.add(second)
-//        if (first != null) second?.neighbors?.add(first)
+
+    fun getneighbours() = this.neighbours
+
+    fun setneighbours(neighbours: MutableList<Node>) {
+        this.neighbours = neighbours
     }
-    fun addVertex(name: Int) {
-        vertices[name] = Vertex(name)
-    }
-    fun connect(first: Int, second: Int) =
-        connect(vertices[first], vertices[second])
-    fun neighbors(name: Int): List<Int> =
-        vertices[name]?.neighbors?.map { it.name } ?: listOf()
 }
 
-var visited: Set<Int> = setOf()
-//fun Graph.dfs(start: Int, finish: Int) =
-//    dfs(start, finish) ?: -1
-fun Graph.dfs(start: Int, finish: Int): Int? =
-    if (start == finish) -1
-    else {
-        visited = visited + start
-        val min = neighbors(start).filter {
-            it !in visited
-        }.map {
-            dfs(it, finish)
-        }.filterNotNull().min()
-        if (min == null) null else min + 1
+fun dfs(node: Node) {
+    val neighbours = node.getneighbours()
+    node.visited = true
+    for (el in neighbours) {
+        if (!el.visited) {
+            dfs(el)
+        }
     }
+}
 
 
 fun main(){
-    var g = Graph()
-
     val n = 1000
-    for (i in 0 until n){
-        g.addVertex(i)
-    }
+    val nodes = Array(n) { i -> Node(i) }
     for (i in 0 until n) {
         for (j in i until n) {
-            g.connect(i, j)
+            nodes[i].addneighbours(nodes[j])
         }
     }
     val time = System.nanoTime()
-    g.dfs(1, 7)
+    dfs(nodes[7])
     val timeMilli = (System.nanoTime() - time) / 1000000.0f
 
     println("$timeMilli")
